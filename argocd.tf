d7cccd7a8ba648ee53373a79a8bbad3d1c233a6d
moses adelere <moses.adelere@gmail.com>
2025-07-05 13:29:36 +0300

    Argocd config

---
+++ argocd.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "argocd" {
+  name = "argocd"
+  namespace = "argocd"
+  repository =  = "https://argoproj.github.io/argo-helm"
+  chart = "argo-cd"
+  create_namespace = "true"
+
+  values = [
+    <<EOF
+    server:
+      services:
+        type: ClusterIP
+    EOF
+  ]
+}
